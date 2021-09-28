2d47705fd526a36b55e02df58c95789419976b6e
Flamir0908123 <bgladki86@gmail.com>
2021-09-28 15:20:08 +0300

    np

--- src/App.js
+++ src/App.js
@@ -1,104 +1,95 @@
-import { Component } from 'react';
-// Components
-import ContactForm from './Components/ContactForm';
-import ContactList from './Components/ContactList';
-import Filter from './Components/Filter';
+import React, { Component } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
 
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
-
-  setContactId = () => {
-    const contactIds = [];
-    this.state.contacts.map(contact =>
-      contactIds.push(contact.id.replace(/\D/g, '')),
-    );
-    return 'id-' + (Math.max(...contactIds) + 1);
-  };
-
-  capitalizeFirstLetter = string => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
-  formSubmitHandler = data => {
-    const { name, number } = data;
-    const normalizedName = name.toLowerCase();
-
-    if (
-      this.state.contacts.find(
-        ({ name }) => name.toLowerCase() === normalizedName,
-      )
-    ) {
-      return alert(`${name} is already in contacts.`);
-    }
-
-    const newContact = {
-      id: this.setContactId(),
-      name: this.capitalizeFirstLetter(name),
-      number,
-    };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  //LOCAL STORAGE START
 
   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
+    //console.log(" component did mount");
+    const contacts = localStorage.getItem("contacts");
     const parsedContacts = JSON.parse(contacts);
+    //console.log(parsedContacts);
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // console.log('Component did update');
-    // console.log(prevState);
-    // console.log(this.state);
-
+    //console.log("component did update");
     if (this.state.contacts !== prevState.contacts) {
-      // console.log('Contacts update');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      //console.log("Контакти було оновлено");
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
     }
   }
 
-  //LOCAL STORAGE END
+  formSubmitHandler = (data) => {
+    this.repeatControl(data);
+  };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  repeatControl = (data) => {
+    let nameArray = [];
+    nameArray = this.state.contacts.map((cur) => cur.name);
+    if (!nameArray.includes(data.name)) {
+      let arrayCont = [];
+      arrayCont = [
+        ...this.state.contacts,
+        { id: uuidv4(), name: data.name, number: data.number },
+      ];
+      return this.setState({ ...this.state, contacts: arrayCont });
+    } else {
+      alert(" Контакт вже є у телефонній книзі!!!");
+    }
+  };
+
+  elementDelete = (arr, idContact) => {
+    let newArr = arr.filter((elem) => elem.id !== idContact);
+    return newArr;
+  };
+
+  deleteContactFromContactList = (idContact) => {
+    let newArrAfterDel = this.elementDelete(this.state.contacts, idContact);
+    this.setState({
+      ...this.state,
+      contacts: [...newArrAfterDel],
+    });
+  };
+
+  setFilterToState = (filterData) => {
+    this.setState({ ...this.state, filter: `${filterData}` });
+  };
+
+  filterArr = (fArr) => {
+    let newArr = fArr.filter((cur) =>
+      cur.name.toUpperCase().includes(this.state.filter)
+    );
+    return newArr;
   };
 
   render() {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = this.state.filter.toLowerCase();
-
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
     return (
-      <div className="phonebook">
-        <h1 className="phonebook_title">Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
-        <h2 className="phonebook_title">Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList contacts={visibleContacts} onDelete={this.deleteContact} />
+      <div className="App">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitData={this.formSubmitHandler} />
+        <h1>Contacts</h1>
+        <Filter setFilterToState={this.setFilterToState} />
+        <ContactList
+          contacts={this.filterArr(this.state.contacts)}
+          del={this.deleteContactFromContactList}
+        />
       </div>
     );
   }
 }
-
 export default App;
